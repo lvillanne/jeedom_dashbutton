90cc4d4c60f6e8ead2f50954054b25e660bf6131
AzraelMonu <l.villanne@gmail.com>
2020-06-11 18:54:24 +0200

    Update nodejs.sh
    Modification du fichier d'installation des dépendances pour faire pointer vers un ancien fichier d'installation qui ne nécessitait que node 8 minimum.

--- resources/nodejs.sh
+++ resources/nodejs.sh
@@ -7,7 +7,7 @@ echo 0 > /tmp/${2}_dep
 
 sudo apt-get install -y libpcap-dev
 
-wget https://raw.githubusercontent.com/lunarok/jeedom_nodejs/master/nodejs.sh -O dependencies.sh
+wget https://raw.githubusercontent.com/lunarok/jeedom_nodejs/f64d88e0dfe04371b20af2815db37e7a1db6462b/nodejs.sh -O dependencies.sh
 sh dependencies.sh ${1} ${2}
 rm dependencies.sh
 
